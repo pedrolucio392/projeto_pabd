07db070d1ec1560e29dade48ef5ebfa02e98dc28
Pedro Lúcio de Azevedo <130012482+pedrolucio392@users.noreply.github.com>
2023-06-27 11:44:11 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+import page.insert as insert
+import page.select as select
+
+#criando a barra lateral do menu
+st.sidebar.title('Menu')
+selectbox = st.sidebar.selectbox('Ação', ['Inserir', 'Consultar'])
+
+if selectbox == 'Inserir':
+    insert.inserir()
+
+if selectbox == 'Consultar':
+    select.consultar()
