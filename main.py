5a273d69fba49b04cf4820a2acfa5861bb06265e
Pedro Lúcio de Azevedo <130012482+pedrolucio392@users.noreply.github.com>
2023-07-04 10:32:24 -0300

    Função adicionar venda

--- main.py
+++ main.py
@@ -5,10 +5,12 @@ import page.select as select
 
 #criando a barra lateral do menu
 st.sidebar.title('Menu')
-selectbox = st.sidebar.selectbox('Ação', ['Inserir', 'Consultar'])
+selectbox = st.sidebar.selectbox('Ação', ['Inserir cliente', 'Inserir produto', 'Inserir cliente', 'Consultar'])
 
 if selectbox == 'Inserir':
     insert.inserir()
 
 if selectbox == 'Consultar':
     select.consultar()
+
+
--- page/insert.py
+++ page/insert.py
@@ -2,8 +2,8 @@ import streamlit as st
 
 import controller.cliente as cliente
 
-def inserir():
-    st.title('Inserir Dados')
+def inserir_cliente():
+    st.title('Inserir Cliente')
     
     with st.form(key='insert'):
         input_cpf = st.number_input(label='Insira o CPF:', format='%d', step=1)
@@ -16,3 +16,20 @@ def inserir():
         if buttom_submit:
             cliente.incluir(input_cpf, input_nome, input_endereco, input_telefone)
             st.success('Cliente incluido com sucesso')
+
+
+def inserir_venda():
+    st.title('Inserir Venda')
+    
+    with st.form(key='insert'):
+        input_ID_venda = st.number_input(label='Insira o ID da venda:', format='%d', step=1)
+        input_data_venda = st.text_input(label='Insira a data da venda:')
+        input_ID_produto = st.text_input(label='Insira id do produto:')
+        input_CNPJ = st.number_input(label='Insira o cnpj da empresa:', format='%d', step=1)
+        input_CPF_cliente = st.number_input(label='Insira o cpf do cliente:', format='%d', step=1)
+
+        buttom_submit = st.form_submit_button('Enviar')
+        
+        if buttom_submit:
+            cliente.incluir(input_ID_venda, input_data_venda, input_ID_produto, input_CNPJ, input_CPF_cliente)
+            st.success('Venda incluída com sucesso.')
